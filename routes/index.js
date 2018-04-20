533972716f433c4249845aa5aa6708867accebae
Ankit Goyal <youremail@domain.com>
2018-04-20 07:27:24 +0530

    routes added for post handleing of fporm

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var submit = require('../app/submit');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { msg: '' });
 });
+
+router.post('/submit_form', submit.submit_form);
 
 module.exports = router;
