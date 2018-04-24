e2c095c23e601f56bb3ee2a1e84648fe8f17df18
Ankit Goyal <youremail@domain.com>
2018-04-24 10:06:19 +0530

    Edit read me

--- app/submit.js
+++ app/submit.js
@@ -1,13 +1,23 @@
 var conn = require('./connection');
 
 exports.submit_form = function(req, res){
+    var error = false;
+    var msg = '';
     var firstName = conn.escape(req.body.firstname);
     var lastName = conn.escape(req.body.lastname);
-    var age = conn.escape(req.body.age);
+    if(req.body.age > 0){
+        var age = conn.escape(req.body.age);
+    } else {
+        error = true;
+        msg = 'age provided is incorrct';
+    }
     var dob = conn.escape(req.body.dob);
     var phone = conn.escape(req.body.phone);
     var gender = conn.escape(req.body.gender);
     var text = conn.escape(req.body.text);
+    if(error){
+        res.render('index', {msg: msg}); 
+    }
     var query = `INSERT INTO form_data (first_name, last_name, age, dob, phone, gender, text) VALUES (${firstName}, ${lastName}, ${age}, ${dob}, ${phone}, ${gender}, ${text})`;
     conn.query(query, function(err, result){
         if(err){
